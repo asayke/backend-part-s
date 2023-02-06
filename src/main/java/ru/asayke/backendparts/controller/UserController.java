f03f2d8431f5b7e1bbad12d8df9d77cc9b22d8d3
oleja <noilzep@gmail.com>
2023-02-06 20:42:12 +0300

    Added CrossOrigin

--- src/main/java/ru/asayke/backendparts/controller/UserController.java
+++ src/main/java/ru/asayke/backendparts/controller/UserController.java
@@ -2,6 +2,7 @@ package ru.asayke.backendparts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import ru.asayke.backendparts.service.UserService;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("api/")
 public class UserController {
